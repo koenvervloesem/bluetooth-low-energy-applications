c92e53ac0c62490c051bf4f2826d0358442e9534
Koen Vervloesem <koen@vervloesem.eu>
2022-01-31 09:43:02 +0100

    Build Arduino sketches in CI

---
+++ scripts/build_arduino.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+DIR=$(pwd)
+
+for directory in 3-advertisements 4-connections 5-security 6-profiles 7-lowpower 8-reverse; do
+  cd "$DIR" || exit
+  cd "$directory" || exit
+  echo Checking "$directory"...
+
+  if [ -d arduino ]; then
+    echo Building Arduino code...
+    cd arduino
+    for i in $(ls); do
+      cd "$i" || exit
+      echo Building "$i"...
+      make build
+      cd ..
+    done
+    cd ..
+  fi
+
+done
